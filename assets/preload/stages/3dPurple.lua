2d49c22a4f070e739be82e3cc071e7914c3632be
matthewfroncek16 <108594640+matthewfroncek16@users.noreply.github.com>
2022-09-17 21:13:53 -0400

    Add files via upload

---
+++ assets/preload/stages/3dPurple.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('purple', 'purple', -500, -150);
+	setScrollFactor('purple', 0.9, 0.9);
+	addGlitchEffect('purple', 2, 5, 0.1)
+	addLuaSprite('purple',false)
+end
